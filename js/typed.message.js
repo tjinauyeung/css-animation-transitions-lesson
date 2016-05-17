5b5fde60d6583383460cd26438105a9847898a8a
Tjin Au Yeung <tjin@usabilla.com>
2016-05-17 19:59:39 +0200

    removed unneeded scripts

--- js/typed.message.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $("#typed").typed({
-      strings: ["Interested?", "Want to know more?", "Get in touch?", "Meow?", "Prrrrrrrrrr?"],
-      typeSpeed: 60,
-      backSpeed: 40,
-      loop: true,
-      showCursor: false
-    });
-});
